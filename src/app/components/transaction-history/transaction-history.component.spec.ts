77d7ef450e75fc283993904c4cf3fe272a443950
Bernardo Marques <bernardohmarques@gmail.com>
2024-12-09 02:08:01 -0300

    draft: unit tests for transaction history

--- src/app/components/transaction-history/transaction-history.component.spec.ts
+++ src/app/components/transaction-history/transaction-history.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TransactionHistoryComponent } from './transaction-history.component';
+import { By } from '@angular/platform-browser';
 
 describe('TransactionHistoryComponent', () => {
   let component: TransactionHistoryComponent;
@@ -8,10 +9,9 @@ describe('TransactionHistoryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TransactionHistoryComponent]
-    })
-    .compileComponents();
-    
+      imports: [TransactionHistoryComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(TransactionHistoryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,10 @@ describe('TransactionHistoryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have table items', async () => {
+    await fixture.whenStable();
+    expect(component.dataSource.filteredData.length).toBe(25);
   });
 });
