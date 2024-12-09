bc38190f54f45821751ddfad6b1095027d8af92c
Bernardo Marques <bernardohmarques@gmail.com>
2024-12-09 05:29:54 -0300

    feat: add filters

--- src/app/components/transaction-history/transaction-history.component.ts
+++ src/app/components/transaction-history/transaction-history.component.ts
@@ -1,23 +1,22 @@
-import {
-  AfterViewInit,
-  Component,
-  inject,
-  OnDestroy,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialog } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { finalize, Subscription } from 'rxjs';
-import { TransactionDetailsComponent } from '../transaction-details/transaction-details.component';
+import { Subscription } from 'rxjs';
 import { Transaction } from '../../models/transactions.model';
 import { TransactionsService } from '../../services/transactions/transactions.service';
-import { CommonModule } from '@angular/common';
-import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
-import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { unformatTransactionDate } from '../../utils/unformatTransactionDate';
+import { TransactionDetailsComponent } from '../transaction-details/transaction-details.component';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-transaction-history',
@@ -28,7 +27,13 @@ import { unformatTransactionDate } from '../../utils/unformatTransactionDate';
     MatProgressSpinnerModule,
     CommonModule,
     MatSortModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    FormsModule,
   ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './transaction-history.component.html',
   styleUrl: './transaction-history.component.scss',
 })
@@ -47,11 +52,8 @@ export class TransactionHistoryComponent implements OnInit, OnDestroy {
   @ViewChild(MatSort) set matSort(ms: MatSort) {
     this.sort = ms;
     this.dataSource.sort = this.sort;
+    this.dataSource.filterPredicate = this.customFilterPredicate();
     this.dataSource.sortingDataAccessor = (row, column) => {
-      console.log(
-        row.transactionDate,
-        unformatTransactionDate(row.transactionDate)
-      );
       if (column === 'transactionDate') {
         return unformatTransactionDate(row.transactionDate).getTime();
       }
@@ -63,6 +65,13 @@ export class TransactionHistoryComponent implements OnInit, OnDestroy {
     new MatTableDataSource();
   transactionsSub!: Subscription;
   isLoading = false;
+  filters = {
+    sourceCurrency: '',
+    targetCurrency: '',
+    transactionDate: null,
+    minValue: null,
+    maxValue: null,
+  };
 
   ngOnInit() {
     this.transactionsSub = this.transactionsService.transactions$.subscribe({
@@ -92,7 +101,6 @@ export class TransactionHistoryComponent implements OnInit, OnDestroy {
   ];
 
   announceSortChange(sortState: Sort) {
-    console.log(sortState);
     if (sortState.direction) {
       this.liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
     } else {
@@ -105,6 +113,47 @@ export class TransactionHistoryComponent implements OnInit, OnDestroy {
       width: '500px',
       data: { id },
     });
+  }
+
+  customFilterPredicate() {
+    return (data: Transaction, filter: string): boolean => {
+      const filters = JSON.parse(filter);
+
+      if (
+        filters.sourceCurrency &&
+        data.sourceCurrency !== filters.sourceCurrency
+      ) {
+        return false;
+      }
+
+      if (
+        filters.targetCurrency &&
+        data.targetCurrency !== filters.targetCurrency
+      ) {
+        return false;
+      }
+
+      if (
+        filters.transactionDate &&
+        unformatTransactionDate(data.transactionDate).toDateString() !==
+          new Date(filters.transactionDate).toDateString()
+      ) {
+        return false;
+      }
+
+      if (filters.minValue !== null && data.paidValue < filters.minValue) {
+        return false;
+      }
+      if (filters.maxValue !== null && data.paidValue > filters.maxValue) {
+        return false;
+      }
+
+      return true;
+    };
+  }
+
+  applyFilter() {
+    this.dataSource.filter = JSON.stringify(this.filters);
   }
 
   ngOnDestroy() {
